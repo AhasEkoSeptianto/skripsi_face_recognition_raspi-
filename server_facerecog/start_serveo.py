5556eb43676d4427254304a74be853e7a73cfb78
AhasEko <ahaseko1@gmail.com>
2023-04-01 15:10:34 +0700

    save server mobile apps host to db

--- server_facerecog/start_serveo.py
+++ server_facerecog/start_serveo.py
@@ -1,10 +1,35 @@
 import subprocess
+from pymongo import MongoClient
+
+# db config
+cluster = MongoClient("mongodb+srv://ahaseko:aaseko100465@cluster0.hqm02.mongodb.net/skripsi?retryWrites=true&w=majority")
+db = cluster['skripsi']
+col = db['raspi_configs']
+
+
 
 command = "ssh -o StrictHostKeyChecking=no -R 80:localhost:3000 serveo.net"
 process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+host = ""
+raspiID = ""
+
+with open('./../raspiID.txt') as f:
+    contents = f.read()
+    raspiID = contents
+
+def SaveServer():
+    data = col.find_one({ "raspi_id": raspiID })
+    if (data):
+        col.find_one_and_update({ "raspi_id": raspiID }, { "$set": {"mobileAppsCon": host}})
+        
+    else:
+        print("raspi not found")
+    
+    
 
 while True:
     output = process.stdout.readline()
+    print(output)
     if not output and process.poll() is not None:
         break
     if output:
@@ -12,4 +37,7 @@ while True:
             print('okeh')
             process.stdin.write(b"yes\n")
             process.stdin.flush()
-        print(output.decode().strip())
+        host = output.decode().strip()
+        
+        SaveServer()
+
